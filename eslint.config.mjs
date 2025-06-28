0347e2329eaf53b658c1d1611beed86aae201d8f
shohaib1996 <khanshohaibhossain@gmail.com>
2025-06-28 10:05:21 +0600

    setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+// eslint.config.mjs
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import tseslint from 'typescript-eslint';
+
+export default [
+  pluginJs.configs.recommended,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-undef': 'error',
+      'no-console': 'warn',
+    },
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+  {
+    ignores: ['node_modules/', 'dist/', 'build/'],
+  },
+];
