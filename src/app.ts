8bac0e44e1835dedc170182dd9e1d09ea9d8e43b
shohaib1996 <khanshohaibhossain@gmail.com>
2025-07-05 09:25:48 +0600

    resolve the cors policy

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,11 @@ const app: Application = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://your-other-address.com'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true, 
+}));
 app.use('/api', router)
 
 app.get('/', (req: Request, res: Response) => {
