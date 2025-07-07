5bb62a6acbeba0351f924c7ad335419f2a75f7a8
shohaib1996 <khanshohaibhossain@gmail.com>
2025-07-08 00:27:34 +0600

    resolve error

--- src/app.ts
+++ src/app.ts
@@ -4,26 +4,21 @@ import router from './app/routes';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 import { notFound } from './app/middlewares/notFound';
 import cookieSession from 'cookie-session';
-import config from './app/config';
 
 const app: Application = express();
 
+app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://form-ai-builder.vercel.app"],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    origin: ['http://localhost:3000', 'https://form-ai-builder.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
-  })
+  }),
 );
-
-app.options("*", cors());
-
-
-app.use(express.json());
 app.use(
   cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
-    keys: [config.SESSION_SECRET as string],
+    keys: [process.env.SESSION_SECRET as string],
   }),
 );
 app.use('/api', router);
